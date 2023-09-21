7d5b648f452cf41c204a2fa6a593b46767fe3553
rafamoral <rafael_moral@yahoo.com.br>
2023-09-21 10:11:20 +0100

    update

---
+++ data_visualisation/data/simple_df.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+
+# make a small data frame
+data_df <- tibble(var1 = c(1, 3, 7, 11),
+                  var2 = c(5, 7, 10, 12),
+                  var3 = c('a', 'b', 'a', 'b')
+)
+
