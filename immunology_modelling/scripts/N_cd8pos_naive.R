ff1d1e69cb68c1d259fc067b9a24dd7a41ad3a59
Rafael Moral <rafael_moral@yahoo.com.br>
2024-04-26 17:08:12 +0200

    update code

--- immunology_modelling/scripts/N_cd8pos_naive.R
+++ immunology_modelling/scripts/N_cd8pos_naive.R
@@ -11,40 +11,31 @@ facs <- read_table("facs_counts_renamed.txt")
 
 cell_expression <- full_join(ecrf, facs)
 
-## monocytes
+## cd8pos_naivecytes
 
-N_mono <- full_join(ecrf, facs %>% dplyr::select(N_mono.panel5, SUBJID)) %>%
+N_cd8pos_naive <- full_join(ecrf, facs %>% dplyr::select(N_CD8bpos_naive.panel1, SUBJID)) %>%
   dplyr::select(- SUBJID)
 
-fit <- gamlss(N_mono.panel5 ~ .,
+fmla_rhs <- paste("~", paste(names(N_cd8pos_naive)[-40], collapse = "+"))
+
+fit <- gamlss(N_CD8bpos_naive.panel1 ~ .,
               sigma.formula = ~ .,
               family = NBI,
-              data = na.omit(N_mono))
+              data = na.omit(N_cd8pos_naive))
 wp(fit)
 summary(fit)
-
-drop1(fit, what = "sigma")
-
-fit2 <- gamlss(N_mono.panel5 ~ .,
-               sigma.formula = ~ LivesWithKids + BMI + HoursOfSleep +
-                 Income + HadRubella + HadTonsillectomy + HeartRate + Temperature,
-               family = NBI,
-               data = na.omit(N_mono))
-
-LR.test(fit2, fit)
 
 ## 5-fold cross-validation
 fold <- gl(5, 156)
 rmse <- numeric(5)
 
 for(i in 1:5) {
-  training_data <- na.omit(N_mono)[- which(fold == i),]
-  fit_fold <- gamlss(N_mono.panel5 ~ .,
-                     sigma.formula = ~ LivesWithKids + BMI + HoursOfSleep +
-                       Income + HadRubella + HadTonsillectomy + HeartRate + Temperature,
+  training_data <- na.omit(N_cd8pos_naive)[- which(fold == i),]
+  fit_fold <- gamlss(N_CD8bpos_naive.panel1 ~ .,
+                     sigma.formula = ~ .,
                      family = NBI,
                      data = training_data)
-  y <- training_data$N_mono.panel5
+  y <- training_data$N_CD8bpos_naive.panel1
   y_hat <- predict(fit_fold, type = "response")
   rmse[i] <- sqrt(sum((y - y_hat)^2))
 }
@@ -57,8 +48,8 @@ fit_control <- trainControl(method = "cv",
                             number = 5)
 
 set.seed(2024)
-rf_fit <- train(N_mono.panel5 ~ .,
-                data = na.omit(N_mono),
+rf_fit <- train(N_CD8bpos_naive.panel1 ~ .,
+                data = na.omit(N_cd8pos_naive),
                 method = "rf",
                 tuneGrid = data.frame(mtry = c(2,5,10,15,20)),
                 trControl = fit_control,
@@ -68,19 +59,13 @@ rf_fit
 varImp(rf_fit)
 
 ## comparing predictions on training set
-y <- na.omit(N_mono)$N_mono.panel5
+y <- na.omit(N_cd8pos_naive)$N_CD8bpos_naive.panel1
 y_hat_nb <- predict(fit, type = "response")
-y_hat_rf <- predict(rf_fit, na.omit(N_mono))
+y_hat_rf <- predict(rf_fit, na.omit(N_cd8pos_naive))
 
 cor(y, y_hat_nb)
 cor(y, y_hat_rf)
 
-library(DescTools)
-CCC(y, y_hat_nb)$rho.c
-CCC(y, y_hat_rf)$rho.c
-
-
 par(mfrow = c(1,2))
 plot(y, y_hat_nb, main = "NB"); abline(0, 1, lty = 2)
 plot(y, y_hat_rf, main = "RF"); abline(0, 1, lty = 2)
-
