7f9f99cea1dddfa213f9ede93540607ed9745d7d
rafamoral <rafael_moral@yahoo.com.br>
2024-07-17 12:48:08 +0100

    update

--- time_series_forecasting/scripts/intro_ts_part5.R
+++ time_series_forecasting/scripts/intro_ts_part5.R
@@ -236,3 +236,176 @@ sim_df %>%
 
 ## reading forest fires data
 
+fires_df <- read_csv("https://raw.githubusercontent.com/rafamoral/courses/main/time_series_forecasting/data/forest_fires.csv")
+
+fires_df %>%
+  ggplot(aes(x = year, y = acres)) +
+  theme_bw() +
+  geom_line()
+
+## fitting an ARCH(1) model
+model_code = "
+model
+{
+  # Likelihood
+  for (t in 1:T) {
+    y[t] ~ dnorm(alpha, sigma[t]^-2)
+  }
+  sigma[1] ~ dunif(0, 1)
+  for(t in 2:T) {
+    sigma[t] <- sqrt(gamma_1 + gamma_2 * pow(y[t-1] - alpha, 2))
+  }
+
+  # Priors
+  alpha ~ dnorm(0.0, 100^-2)
+  gamma_1 ~ dunif(0, 100)
+  gamma_2 ~ dunif(0, 1)
+}
+"
+
+ff_run <- jags(data = list(y = scale(fires_df$acres)[,1],
+                           T = nrow(fires_df)),
+               parameters.to.save = c("sigma", "alpha", "gamma_1", "gamma_2"),
+               model.file=textConnection(model_code))
+
+plot(ff_run)
+
+par(mfrow=c(1,2))
+hist(ff_run$BUGSoutput$sims.list$gamma_1, breaks = 30)
+hist(ff_run$BUGSoutput$sims.list$gamma_2, breaks = 30)
+
+## posterior standard deviations
+sigma_post <- ff_run$BUGSoutput$sims.list$sigma
+
+fires_df <- fires_df %>%
+  mutate(sigma_hat = c(NA, ff_run$BUGSoutput$median$sigma[-1]),
+         upper = c(NA, apply(sigma_post, 2, quantile, 0.975)[-1]),
+         lower = c(NA, apply(sigma_post, 2, quantile, 0.025)[-1]))
+
+fires_df %>%
+  ggplot(aes(x = year, y = sigma_hat)) +
+  theme_bw() +
+  geom_line() +
+  geom_ribbon(aes(ymin = lower,
+                  ymax = upper),
+              alpha = .2)
+
+## fitting a GARCH(1,1) model
+
+model_code = "
+model
+{
+  # Likelihood
+  for (t in 1:T) {
+    y[t] ~ dnorm(alpha, sigma[t]^-2)
+  }
+  sigma[1] ~ dunif(0,10)
+  for(t in 2:T) {
+    sigma[t] <- sqrt(gamma_1 + gamma_2 * pow(y[t-1] - alpha, 2) 
+                        + gamma_3 * pow(sigma[t-1], 2))
+  }
+  # Priors
+  alpha ~ dnorm(0, 10^-2)
+  gamma_1 ~ dunif(0, 10)
+  gamma_2 ~ dunif(0, 1)
+  gamma_3 ~ dunif(0, 1)
+}
+"
+
+ff_run_2 <- jags(data = list(y = scale(fires_df$acres)[,1],
+                             T = nrow(fires_df)),
+                 parameters.to.save = c("sigma", "alpha",
+                                        "gamma_1", "gamma_2", "gamma_3"),
+                 model.file=textConnection(model_code))
+
+plot(ff_run_2)
+
+hist(ff_run_2$BUGSoutput$sims.list$gamma_1, breaks = 30, xlab = 'gamma 1')
+hist(ff_run_2$BUGSoutput$sims.list$gamma_2, breaks = 30, xlab = 'gamma 2')
+hist(ff_run_2$BUGSoutput$sims.list$gamma_3, breaks = 30, xlab = 'gamma 3')
+
+## posterior standard deviations
+sigma_post <- ff_run_2$BUGSoutput$sims.list$sigma
+
+fires_df <- fires_df %>%
+  mutate(sigma_hat = c(NA, ff_run_2$BUGSoutput$median$sigma[-1]),
+         upper = c(NA, apply(sigma_post, 2, quantile, 0.975)[-1]),
+         lower = c(NA, apply(sigma_post, 2, quantile, 0.025)[-1]))
+
+fires_df %>%
+  ggplot(aes(x = year, y = sigma_hat)) +
+  theme_bw() +
+  geom_line() +
+  geom_ribbon(aes(ymin = lower,
+                  ymax = upper),
+              alpha = .2)
+
+## comparing with DIC
+r_1 <- print(ff_run)
+r_2 <- print(ff_run_2)
+with(r_1, print(c(DIC, pD)))
+with(r_2, print(c(DIC, pD)))
+
+## Fitting a stochastic volatility model in JAGS
+
+model_code = "
+model
+{
+  # Likelihood
+  for (t in 1:T) {
+    y[t] ~ dnorm(alpha, sigma_h[t]^-2)
+    sigma_h[t] <- sqrt(exp(h[t]))
+  }
+  h[1] <- mu
+  for(t in 2:T) {
+    h[t] ~ dnorm(mu + phi * h[t-1], sigma^-2)
+  }
+
+  # Priors
+  alpha ~ dnorm(0, 100^-2)
+  mu ~ dnorm(0, 100^-2)
+  phi ~ dunif(-1, 1)
+  sigma ~ dunif(0,100)
+}
+"
+
+ff_run_3 <- jags(data = list(y = scale(fires_df$acres)[,1],
+                             T = nrow(fires_df)),
+                 parameters.to.save = c("alpha", "mu",
+                                        "phi", "sigma", "h"),
+                 model.file=textConnection(model_code))
+
+plot(ff_run_3)
+
+hist(ff_run_3$BUGSoutput$sims.list$alpha, breaks = 30)
+hist(ff_run_3$BUGSoutput$sims.list$mu, breaks = 30)
+hist(ff_run_3$BUGSoutput$sims.list$phi, breaks = 30)
+hist(ff_run_3$BUGSoutput$sims.list$sigma, breaks = 30)
+
+## posterior process h
+h_post <- ff_run_3$BUGSoutput$sims.list$h
+
+fires_df <- fires_df %>%
+  mutate(h_hat = c(NA, ff_run_3$BUGSoutput$median$h[-1]),
+         upper = c(NA, apply(h_post, 2, quantile, 0.975)[-1]),
+         lower = c(NA, apply(h_post, 2, quantile, 0.025)[-1]))
+
+fires_df <- fires_df %>%
+  mutate(h_hat = sqrt(exp(h_hat)),
+         upper = sqrt(exp(upper)),
+         lower = sqrt(exp(lower)))
+
+fires_df %>%
+  ggplot(aes(x = year, y = h_hat)) +
+  theme_bw() +
+  geom_line() +
+  geom_ribbon(aes(ymin = lower,
+                  ymax = upper),
+              alpha = .2)
+
+## comparison
+r_3 <- print(ff_run_3)
+
+with(r_1, print(c(DIC, pD)))
+with(r_2, print(c(DIC, pD)))
+with(r_3, print(c(DIC, pD))) ## much better fit but a lot more parameters!
