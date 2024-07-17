efd1df3e6b1d9d89165a4278f62449c92c051582
rafamoral <rafael_moral@yahoo.com.br>
2024-07-17 14:00:52 +0100

    update

--- time_series_forecasting/scripts/intro_ts_part6.R
+++ time_series_forecasting/scripts/intro_ts_part6.R
@@ -127,3 +127,87 @@ with(ou_dic, print(c(DIC, pD)))
 ## vector autoregression
 
 ## reading the temperature and sea level datasets
+
+# HadCRUT is the dataset of worldwide monthly instrumental temperature
+# records formed by combining the sea surface temperature records compiled
+# by the Hadley Centre of the UK Met Office and the land surface air
+# temperature records compiled by the Climatic Research Unit (CRU) of
+# the University of East Anglia.
+
+temp_df <- read_csv("https://raw.githubusercontent.com/rafamoral/courses/main/time_series_forecasting/data/hadcrut.csv")
+
+temp_df %>%
+  ggplot(aes(x = Year, y = Anomaly)) +
+  theme_bw() +
+  geom_line()
+
+sealevel_df <- read_csv("https://raw.githubusercontent.com/rafamoral/courses/main/time_series_forecasting/data/tide_gauge.csv")
+## correcting sea level ages
+sealevel_df <- sealevel_df %>%
+  mutate(Year2 = year_AD - 0.5)
+
+## merge them together
+bivariate_df <- merge(temp_df, sealevel_df, by.x='Year', by.y='Year2') %>%
+  dplyr::select(Year, Anomaly, SeaLevel = sea_level_m)
+
+## fitting VAR(1) model
+
+model_code = "
+model
+{
+  # Likelihood
+  for (t in 2:T) {
+    y[t, ] ~ dmnorm(mu[t, ], Sigma.Inv[,])
+    mu[t, 1:k] <- A + Phi %*% y[t-1,]
+  }
+  Sigma <- inverse(Sigma.Inv[,]) 
+
+  # Priors
+  Sigma.Inv ~ dwish(I, k+1)
+  for(i in 1:k) {
+    A[i] ~ dnorm(0, 20^-2)
+    for(j in 1:k) {
+      Phi[i,j] ~ dunif(-1,1)
+    }
+  }
+}
+"
+
+n_forecast <- 10
+
+## we will difference the data for VAR
+jags_data <- with(bivariate_df,
+                  list(T = nrow(bivariate_df) + n_forecast - 1,
+                       y = rbind(as.matrix(apply(bivariate_df[,c("Anomaly", "SeaLevel")], 2, "diff")),
+                                 matrix(NA, ncol = 2, nrow = n_forecast)),
+                       k = 2, ## number of time series
+                       I = diag(2)))
+
+var_run <- jags(data = jags_data,
+                parameters.to.save = c("A","Phi","y"),
+                model.file = textConnection(model_code))
+
+plot(var_run)
+
+y_future_post <- var_run$BUGSoutput$sims.list$y
+dim(y_future_post) ## a 3-d array, MCMC samples x N x K
+y_future_med <- apply(y_future_post, c(2,3), "median")
+year_all <- c(bivariate_df$Year[-1], 2010:(2010 + n_forecast))
+
+bivariate_df_pred <- tibble(Year = year_all[-1],
+                            Anomaly = bivariate_df$Anomaly[1] + cumsum(y_future_med[,1]),
+                            SeaLevel = bivariate_df$SeaLevel[1] + cumsum(y_future_med[,2]))
+
+bivariate_df %>%
+  pivot_longer(2:3,
+               names_to = "variable",
+               values_to = "y") %>%
+  ggplot(aes(x = Year, y = y)) +
+  theme_bw() +
+  geom_point(size = .2) +
+  facet_wrap(~ variable) +
+  geom_line(data = bivariate_df_pred %>%
+              pivot_longer(2:3,
+                           names_to = "variable",
+                           values_to = "y"),
+            col = 4)
